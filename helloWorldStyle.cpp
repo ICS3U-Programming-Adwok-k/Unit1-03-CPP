a03a58f98b37a66e5e2639bc4fc184e33abde0a7
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-02-13 20:23:54 +0000

    helloWorldstyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Adwok Adiebo All rights reserved.
+// .
+// Created by: Adwok Adiebo
+// Date: Feb 13 2025
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
